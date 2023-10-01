6b7b16e7f20c1c534c2d4618c71c6939ccb23d0f
ravicant <49905280+ravicant@users.noreply.github.com>
2023-10-02 04:11:33 +0530

    some changes
    some changes

--- js/app.js
+++ js/app.js
@@ -549,7 +549,7 @@ $(function() {
 
 function printArray() {
 	var msg = 'Submit new regions here:\n'
-	+ 'https://github.com/skyrossm/np-gangmap/issues\n\n'
+	+ 'https://discord.com/channels/736106855524335636/1158161626302791853/1158161718254510191\n\n'
 	+ 'Right click the map to add points to the region. You may have to toggle regions off to be able to right click on the bottom layer. Fill in the values marked "<edit here>" and title the new issue using the format: "Add <title> region". Copy and paste everything below this. If your browser does not support selecting the text below press F12 to open the developer console and copy it from there. (scroll down)\n\n';
 	msg += '```json\n\t{\n\t\t"type": "Territories",'
 	+ '\n\t\t"title": "<edit this>",'
